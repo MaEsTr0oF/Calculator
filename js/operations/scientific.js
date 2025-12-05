678820692d6718583c875e72e75189f7ef691d1e
heartinice <ims1018@mail.ru>
2025-12-05 20:11:16 +0300

    Add operations as sin, pow, sqrt, cos

---
+++ js/operations/scientific.js
@@ -0,0 +1,4 @@
+export function sin(a) { return Math.sin(a); }
+export function cos(a) { return Math.cos(a); }
+export function pow(a, b) { return Math.pow(a, b); }
+export function sqrt(a) { return Math.sqrt(a); }
